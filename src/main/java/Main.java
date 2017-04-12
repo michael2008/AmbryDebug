c19e320eed0ef59bc6d8e4e9d4e379ed4a8e2219
jianjun.xie <jianjunxie@81biz.com>
2017-04-12 10:53:20 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,15 @@ import java.io.ByteArrayInputStream;
 
 public class Main {
 
-    private static final int HIT_BLOCK_SIZE = 5 * 1024 * 1024;
+    // 4 * 1024 * 1024 is problematic on newer versions, and other sizes are ok on all versions.
+    private static final int HIT_BLOCK_SIZE = 4 * 1024 * 1024;
+
+    private static final String AMBRY_ENDPOINT = "http://192.168.2.34";
 
     public static void main(String[] args) {
         byte[] block = new byte[HIT_BLOCK_SIZE];
         AmbryFileMeta meta = new AmbryFileMeta(block.length, "HoloCloud", "application/octet-stream");
-        AmbryClient ambryClient = new AmbryClient("http://192.168.2.34", 1174);
+        AmbryClient ambryClient = new AmbryClient(AMBRY_ENDPOINT, 1174);
         try (AmbryPostFileResponse response = ambryClient.postFile(new ByteArrayInputStream(block), meta)) {
             if (!response.isSucceeded()) {
                 throw new RuntimeException("OSS save failed + " + response.getMessage());
